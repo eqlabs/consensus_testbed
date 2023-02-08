eacd7f752c6b7db0c693b31af33f4f980fb5191d
Jos Dehaes <jos.dehaes@gmail.com>
2023-02-08 11:18:26 +0100

    compiles, starts nodes, doesn't start gRPC

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,141 @@
 mod message;
 mod node;
 
-use std::time::Duration;
-
+use ::node::NodeStorage;
+use arc_swap::ArcSwap;
+use config::{
+    AnemoParameters, Authority, Committee, NetworkAdminServerParameters, Parameters,
+    PrometheusMetricsParameters, WorkerCache, WorkerId, WorkerIndex, WorkerInfo,
+};
+use crypto::PublicKey;
+use fastcrypto::{bls12381::min_sig::BLS12381KeyPair, ed25519::Ed25519KeyPair, traits::KeyPair};
+use multiaddr::Multiaddr;
 use pea2pea::{connect_nodes, Topology};
+use std::{collections::BTreeMap, str::FromStr, sync::Arc, time::Duration};
+use sui_types::crypto::{get_key_pair_from_rng, AuthorityKeyPair, NetworkKeyPair};
 use tokio::time::sleep;
+use tracing::debug;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
 
 use crate::node::Node;
 
 // The number of nodes to spawn.
-const NUM_NODES: u16 = 10;
+const NUM_NODES: u16 = 2;
 // The amount of time in seconds the nodes are to be running.
 const RUNTIME_SECS: u64 = 60;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), eyre::Report> {
     // Start the logger.
     start_logger(LevelFilter::INFO);
 
+    // parameters
+    let block_sync_params = config::BlockSynchronizerParameters::default();
+    let consensus_api_grpc = config::ConsensusAPIGrpcParameters {
+        socket_addr: Multiaddr::from_str("/ip4/0.0.0.0/tcp/0/http").unwrap(),
+        get_collections_timeout: Duration::from_secs(5000),
+        remove_collections_timeout: Duration::from_secs(5000),
+    };
+    let prometheus_metrics = PrometheusMetricsParameters {
+        socket_addr: Multiaddr::from_str("/ip4/0.0.0.0/tcp/0/http").unwrap(),
+    };
+    let network_admin_server = NetworkAdminServerParameters {
+        primary_network_admin_server_port: 0,
+        worker_network_admin_server_base_port: 0,
+    };
+    let anemo = AnemoParameters::default();
+    let parameters = Parameters {
+        header_num_of_batches_threshold: 32,
+        min_header_delay: Duration::from_millis(100),
+        max_header_num_of_batches: 1000,
+        max_header_delay: Duration::from_secs(200),
+        gc_depth: 10,
+        sync_retry_delay: Duration::from_secs(10),
+        sync_retry_nodes: 3,
+        batch_size: 500000,
+        max_batch_delay: Duration::from_millis(200),
+        block_synchronizer: block_sync_params,
+        consensus_api_grpc,
+        max_concurrent_requests: 500000,
+        prometheus_metrics,
+        network_admin_server,
+        anemo,
+    };
+    debug!("parameters: {:?}", parameters);
+
+    // Create the committee
+    let mut primary_keys = Vec::new();
+    for _ in 0..NUM_NODES {
+        let primary_keypair: AuthorityKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
+        primary_keys.push(primary_keypair);
+    }
+    let mut network_keys = Vec::new();
+    for _ in 0..NUM_NODES {
+        let network_keypair: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
+        network_keys.push(network_keypair);
+    }
+    let mut worker_keys = Vec::new();
+    for _ in 0..NUM_NODES {
+        let worker_keypair: NetworkKeyPair = get_key_pair_from_rng(&mut rand::rngs::OsRng).1;
+        worker_keys.push(worker_keypair);
+    }
+
+    // create the committee
+    let c = create_committee(&primary_keys, &network_keys);
+    debug!("committee : {:?}", c);
+    let committee = Arc::new(ArcSwap::from_pointee(c));
+
+    // create the worker cache
+    let w = create_worker_cache(&primary_keys, &worker_keys);
+    debug!("worker cache: {:?}", w);
+    let worker_cache = Arc::new(ArcSwap::from_pointee(w));
+
+    debug!("committee: {:?}", &committee);
+
     // Create a number of nodes.
     let mut nodes = Vec::with_capacity(NUM_NODES as usize);
-    for _ in 0..NUM_NODES {
-        nodes.push(Node::new("node_name".to_string(), NUM_NODES - 1).await);
+    for i in 0..NUM_NODES as usize {
+        nodes.push(
+            Node::new(
+                "node_name".to_string(),
+                NUM_NODES - 1,
+                primary_keys.get(i).unwrap(),
+            )
+            .await,
+        );
     }
 
     // Connect the nodes in a dense mesh.
     connect_nodes(&nodes, Topology::Mesh).await.unwrap();
 
     // Start the consensus for all the nodes.
-    for node in &nodes {
-        node.start_consensus();
+    let store_path = "store";
+    let store = NodeStorage::reopen(store_path);
+    let mut handles = Vec::new();
+    for (i, node) in nodes.into_iter().enumerate() {
+        let committee = committee.clone();
+        let store = store.clone();
+        let p = parameters.clone();
+        let pk = primary_keys.remove(0);
+        let nk = network_keys.remove(0);
+        let wk = worker_keys.remove(0);
+        let s = store.clone();
+        let c = committee.clone();
+        let wc = worker_cache.clone();
+        let h = tokio::spawn(async move {
+            let (primary, worker) = node.start_consensus(i, pk, nk, wk, p, s, c, wc).await.unwrap();
+            primary.wait().await;
+            worker.wait().await;
+        });
+        handles.push(h);
     }
 
     // Wait for a desired amount of time to allow the nodes to do some work.
     sleep(Duration::from_secs(RUNTIME_SECS)).await;
+    for h in handles {
+        h.await?;
+    }
+    Ok(())
 }
 
 // Starts logging with the desired log level.
@@ -51,4 +152,55 @@ fn start_logger(default_level: LevelFilter) {
         .without_time()
         .with_target(false)
         .init();
+}
+
+/// create the `Committee` up front from the keys
+fn create_committee(
+    primary_keys: &[BLS12381KeyPair],
+    network_keys: &[Ed25519KeyPair],
+) -> Committee {
+    let mut authorities = BTreeMap::<PublicKey, Authority>::new();
+    let start_port = 3000usize;
+    for i in 0..NUM_NODES as usize {
+        let addr = format!("/ip4/127.0.0.1/udp/{}", start_port + i);
+        authorities.insert(
+            primary_keys.get(i).unwrap().public().clone(),
+            Authority {
+                stake: 1000,
+                primary_address: Multiaddr::from_str(addr.as_str()).unwrap(),
+                network_key: network_keys.get(i).unwrap().public().clone(),
+            },
+        );
+    }
+    Committee {
+        authorities,
+        epoch: 0,
+    }
+}
+
+/// create the `WorkerCache` up front from the keys
+fn create_worker_cache(
+    primary_keys: &[BLS12381KeyPair],
+    worker_keys: &[Ed25519KeyPair],
+) -> WorkerCache {
+    let mut workers = BTreeMap::<PublicKey, WorkerIndex>::new();
+    let mut port = 3008usize;
+    for i in 0..NUM_NODES as usize {
+        let worker = format!("/ip4/127.0.0.1/udp/{port}");
+        port += 1;
+        let transactions = format!("/ip4/127.0.0.1/tcp/{port}/http");
+        port += 1;
+        let mut worker_info = BTreeMap::<WorkerId, WorkerInfo>::new();
+        worker_info.insert(
+            0,
+            WorkerInfo {
+                name: worker_keys.get(i).unwrap().public().clone(),
+                transactions: Multiaddr::from_str(transactions.as_str()).unwrap(),
+                worker_address: Multiaddr::from_str(worker.as_str()).unwrap(),
+            },
+        );
+        let worker_index = WorkerIndex(worker_info);
+        workers.insert(primary_keys.get(i).unwrap().public().clone(), worker_index);
+    }
+    WorkerCache { workers, epoch: 0 }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,9 +1,35 @@
-use std::{io, net::SocketAddr};
+use std::{
+    borrow::Borrow,
+    collections::HashSet,
+    io,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
+use arc_swap::ArcSwap;
+use config::{Committee, Parameters, WorkerCache};
+use crypto::{NetworkKeyPair, PublicKey};
+use fastcrypto::{
+    bls12381::min_sig::BLS12381KeyPair,
+    traits::{KeyPair, ToFromBytes},
+};
+use mysten_metrics::RegistryService;
+use node::{
+    execution_state::SimpleExecutionState, primary_node::PrimaryNode, worker_node::WorkerNode,
+};
 use pea2pea::{
     protocols::{Handshake, Reading, Writing},
     Config, Connection, ConnectionSide, Node as Pea2PeaNode, Pea2Pea,
 };
+use prometheus::Registry;
+use storage::NodeStorage;
+use sui_types::crypto::AuthorityKeyPair;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    sync::mpsc::channel,
+};
+use tracing::debug;
+use worker::TrivialTransactionValidator;
 
 use crate::message::{ConsensusCodec, ConsensusMessage};
 
@@ -11,8 +37,9 @@ use crate::message::{ConsensusCodec, ConsensusMessage};
 pub(crate) struct Node {
     node: Pea2PeaNode,
     // consensus
-    // likely own key
+    pub(crate) own_keypair: Arc<BLS12381KeyPair>,
     // likely peer keys
+    pub(crate) other_keys: Arc<Mutex<HashSet<PublicKey>>>,
 }
 
 impl Pea2Pea for Node {
@@ -23,15 +50,22 @@ impl Pea2Pea for Node {
 
 impl Node {
     /// Creates and starts a new node.
-    pub(crate) async fn new(name: String, max_connections: u16) -> Self {
+    pub(crate) async fn new(
+        name: String,
+        max_connections: u16,
+        primary_keypair: &AuthorityKeyPair,
+    ) -> Self {
         let config = Config {
             name: Some(name),
             max_connections,
             ..Default::default()
         };
+        let kp = AuthorityKeyPair::from_bytes(primary_keypair.as_bytes()).unwrap();
 
         let node = Self {
             node: Pea2PeaNode::new(config),
+            own_keypair: Arc::new(kp),
+            other_keys: Arc::new(Mutex::new(HashSet::new())),
         };
 
         node.enable_handshake().await;
@@ -43,9 +77,51 @@ impl Node {
     }
 
     /// Spawn a task handling the consensus loop.
-    pub(crate) fn start_consensus(&self) {
-        let node = self.clone();
-        tokio::spawn(async move { loop {} });
+    pub(crate) async fn start_consensus(
+        &self,
+        id: usize,
+        primary_keypair: AuthorityKeyPair,
+        network_keypair: NetworkKeyPair,
+        worker_keypair: NetworkKeyPair,
+        parameters: Parameters,
+        store: NodeStorage,
+        committee: Arc<ArcSwap<Committee>>,
+        worker_cache: Arc<ArcSwap<WorkerCache>>,
+    ) -> Result<(PrimaryNode, WorkerNode), eyre::Report> {
+        // let node = self.clone();
+        let (_tx_transaction_confirmation, _rx_transaction_confirmation) = channel(100);
+
+        let registry_service = RegistryService::new(Registry::new());
+        let primary_pub = self.own_keypair.public().clone();
+        let primary = PrimaryNode::new(parameters.clone(), true, registry_service);
+        primary
+            .start(
+                primary_keypair,
+                network_keypair,
+                committee.clone(),
+                worker_cache.clone(),
+                &store,
+                Arc::new(SimpleExecutionState::new(_tx_transaction_confirmation)),
+            )
+            .await?;
+        debug!("created primary id {}", id);
+
+        let registry_service = RegistryService::new(Registry::new());
+        let worker = WorkerNode::new(0, parameters.clone(), registry_service);
+        worker
+            .start(
+                primary_pub.clone(),
+                worker_keypair,
+                committee.clone(),
+                worker_cache,
+                &store,
+                TrivialTransactionValidator::default(),
+                None,
+            )
+            .await?;
+        debug!("created worker id {}", id);
+
+        Ok((primary, worker))
     }
 }
 
@@ -54,7 +130,16 @@ impl Handshake for Node {
     async fn perform_handshake(&self, mut conn: Connection) -> io::Result<Connection> {
         let stream = self.borrow_stream(&mut conn);
 
-        /* exchange any keys */
+        let borrow: &BLS12381KeyPair = self.own_keypair.borrow();
+        let public = borrow.public().clone();
+        let mut src = public.as_bytes();
+        stream.write_all_buf(&mut src).await?;
+
+        let mut buf = [0u8; 1024];
+        stream.read(&mut buf).await?;
+        let other = PublicKey::from_bytes(&buf[..96]).unwrap();
+        let others: &Mutex<HashSet<PublicKey>> = self.other_keys.borrow();
+        others.lock().unwrap().insert(other);
 
         Ok(conn)
     }
@@ -69,10 +154,15 @@ impl Reading for Node {
         Default::default()
     }
 
-    async fn process_message(&self, _source: SocketAddr, message: Self::Message) -> io::Result<()> {
-        match message {
-            _ => todo!(),
-        }
+    async fn process_message(
+        &self,
+        _source: SocketAddr,
+        _message: Self::Message,
+    ) -> io::Result<()> {
+        // match message {
+        //     _ => todo!(),
+        // }
+        Ok(())
     }
 }
 
